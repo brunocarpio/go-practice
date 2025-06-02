0f0ce72623ed6eeebb29cfdd85d09c53737e65dd
Bruno Carpio <bcarpio181@gmail.com>
2025-06-02 01:05:15 -0300

    add hydra logger

--- hydra/main.go
+++ hydra/main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"fmt"
+	"hydra/hlogger"
 	"net/http"
 )
 
 func sroot(w http.ResponseWriter, r *http.Request) {
+	logger := hlogger.GetInstance()
+	logger.Println("Received an http request on root resource")
+
 	fmt.Fprintf(w, "Welcome to the hydra software system")
 }
 
 func main() {
+	logger := hlogger.GetInstance()
+	logger.Println("Starting Hydra web server")
+
 	http.HandleFunc("/", sroot)
 	http.ListenAndServe(":8080", nil)
 }
